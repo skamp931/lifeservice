4a29f1ef5fff7e208206290bb19c684afc21e3f6
skamp931 <43207451+skamp931@users.noreply.github.com>
2023-12-17 23:33:48 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
 st.write("テストページ")
-st.sidebar(
-  "サイドバー",
-  ("a","b","c")
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
 )
+
+# Using "with" notation
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
