a6b07bf253247fabf97ce60bd6cb1a15e9c27097
skamp931 <43207451+skamp931@users.noreply.github.com>
2023-12-17 23:46:11 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ st.write("テストページ")
 # Using object notation
 with st.sidebar:
     add_selectbox = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    "マネー情報",
+    ("幼稚園", "小学生", "高校生", "大学制")
 )
 
 # Using "with" notation
@@ -15,3 +15,15 @@ with st.sidebar:
         "Choose a shipping method",
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
+
+if add_selectbox =="幼稚園":
+    st.write("幼稚園の時にかかる費用")
+    st.write("園児")
+
+if add_selectbox =="小学生":
+    st.write("小学生の時にかかる費用")
+    st.write("小学")
+
+if add_selectbox =="高校生":
+    st.write("高校生の時にかかる費用")
+    st.write("高校")
