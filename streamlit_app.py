c675c7d6de4d2f3ca24276d0130e210358287b9b
skamp931 <43207451+skamp931@users.noreply.github.com>
2023-12-22 00:19:24 +0900

    streamlit_app.py の更新

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ with st.sidebar:
 #    with st.echo():
 #        st.write("This code will be printed to the sidebar.")
 
-    with st.spinner("Loading..."):
+#    with st.spinner("Loading..."):
 #        time.sleep(5)
     st.success("Done!")
     
